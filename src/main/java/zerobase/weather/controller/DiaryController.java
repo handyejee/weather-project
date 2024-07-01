a1683d137d9194e5d2284d0e2a41f3399c7d0b41
yjson <handyejee14@gmail.com>
2024-07-01 22:40:17 +0900

    Swagger 파라미터별 설명 추가, 엔드포인트 별 설명 Operation 어노테이션으로 변경

--- src/main/java/zerobase/weather/controller/DiaryController.java
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -1,6 +1,8 @@
 package zerobase.weather.controller;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController // 상태코드 지정할 수 있는 RestController
+@Tag(name= "날씨일기 API", description = "날씨일기를 CRUD 할 수 있는 백엔드 API 입니다") // 컨트롤러 클래스, API 그룹에 대한 설명 제공하는데 사용
 public class DiaryController {
     private final DiaryService diaryService;
 
@@ -18,10 +21,9 @@ public class DiaryController {
         this.diaryService = diaryService;
     }
 
-
     // client와 맡닿아 있음 : controller - service -domain
     // 1. 날씨 일기 작성하는 api - path 지정하기
-    @Tag(name = "날씨일기 작성 API", description = "날씨 일기 작성을 도와주는 API 입니다")
+    @Operation(summary = "날씨일기 작성 API", description = "날씨 일기 작성을 도와주는 API 입니다")
     @PostMapping("/create/diary")
     void createDiary(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
@@ -32,17 +34,21 @@ public class DiaryController {
     }
 
     // 일기를 날짜에 따라 조회
-    @Tag(name = "날씨일기 단건조회 API", description = "날씨 일기를 선택한 날짜별로 조회하는 API입니다.")
+    @Operation(summary = "날씨일기 단건조회 API", description = "날씨 일기를 선택한 날짜별로 조회하는 API입니다.")
     @GetMapping("/read/diary")
     List<Diary> readDiary(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            @Parameter(description = "조회할 날짜", example = "2024-06-20") LocalDate date) {
         return diaryService.readDiary(date);
     }
 
     // 일기 날짜 범위에 따라 조회
-    @Tag(name = "날씨일기 조회 API", description = "선택한 기간중의 모든 일기 데이터를 가져옵니다")
-    @Schema(description = "날짜 형식 : yyyy-MM-dd", example = "2024-07-01")
+    @Operation(summary = "날씨일기 조회 API", description = "선택한 기간중의 모든 일기 데이터를 가져옵니다")
     @GetMapping("/read/diaries")
+    @Parameters({
+            @Parameter(name = "startDate", description = "조회할 기간의 첫번째 날", example = "2024-06-20"),
+            @Parameter(name = "endDate", description = "조회할 기간의 마지막날", example = "2024-07-01")
+    })
     List<Diary> readDiaries(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
@@ -51,22 +57,24 @@ public class DiaryController {
     }
 
     // 일기 수정 api
-    @Tag(name = "날씨일기 수정 API", description = "날씨 일기 내용을 수정하는 API 입니다")
+    @Operation(summary = "날씨일기 수정 API", description = "날씨 일기 내용을 수정하는 API 입니다")
     @PutMapping("/update/diary")
     void updateDiary(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            @Parameter(description = "수정할 날짜", example = "2024-06-20") LocalDate date,
             @RequestBody String text
 
     ) {
         diaryService.updateDiary(date, text);
     }
 
-    @Tag(name = "날씨일기 삭제 API", description = "작성한 날씨일기를 삭제할 수 있는 API 입니다")
+    @Operation(summary = "날씨일기 삭제 API", description = "작성한 날씨일기를 삭제할 수 있는 API 입니다")
     @DeleteMapping("/delete/diary")
     void deleteDiary(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date)
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            @Parameter(description = "삭제할 날짜", example = "2024-06-20")
+            LocalDate date)
     {
         diaryService.deleteDiary(date);
     }
-
 }
